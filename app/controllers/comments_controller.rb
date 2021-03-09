172b3fd958b03abee30e1979182aeaf48bcf5544
Michael Iodice <miodice3@gmail.com>
2021-03-09 10:53:28 -0700

    check routes, configured.  implement basic controller actions to validate display of json data, confirm it is what was expected

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+
+    def create
+        byebug
+        comment = Comment.create(comment_params)
+    end
+
+    def index
+        comments = Comment.all
+        render json: comments
+    end
+
+    # def show
+    # end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:fedid, :comment)
+    end
+
 end
