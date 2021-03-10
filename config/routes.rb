431c23e0a845785e92d89ddb05af8599c9fb1a13
Michael Iodice <miodice3@gmail.com>
2021-03-10 08:32:28 -0700

    enable CORS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
+  # resources :comments
+  # get '/patients/:id', to: 'patients#show'
+  get '/comments', to: 'comments#index'
+  post '/comments', to: 'comments#create'
 end
